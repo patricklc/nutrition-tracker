efe3fca6d7780f93867be9ae87cad434a8002cc4
Patrick Short <pjsy8c@gmail.com>
2017-05-04 14:51:29 -0500

    Search and display results

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -2,10 +2,16 @@ package org.launchcode.controllers;
 
 import org.launchcode.models.List;
 import org.launchcode.models.Search;
+import org.launchcode.models.forms.SearchForm;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -14,12 +20,25 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class SearchController {
 
+    @RequestMapping(value="")
+    public String displaySearchForm(Model model) {
+        model.addAttribute(new SearchForm());
+        model.addAttribute("title", "Search");
+        return "search-list";
+    }
+    @RequestMapping(value="results")
+    public String displaySearchResults(Model model
+            , @ModelAttribute SearchForm searchForm
+    ) {
 
-    @RequestMapping
-    public String displaySearchFoods(Model model, RestTemplate restTemplate) {
-        Search search = restTemplate.getForObject("https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=DEMO_KEY", Search.class);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.nal.usda.gov/ndb/search/?format=json&q={q}&sort=n&max=25&offset=0&api_key=baLegQMMRlZ8D82V5RiI29hTtDHNviv0YLu6El3D";
+
+        String k = searchForm.getKeyword();
+        Search search =
+                restTemplate.getForObject(url, Search.class, k);
         model.addAttribute("title", "Search Results");
         model.addAttribute("search", search);
-        return "search/list";
+        return "search-list";
     }
 }
---
+++ src/main/java/org/launchcode/models/forms/SearchForm.java
@@ -0,0 +1,16 @@
+package org.launchcode.models.forms;
+
+/**
+ * Created by Patrick on 4/24/2017.
+ */
+public class SearchForm {
+    private String keyword;
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
